5705490898a084b257c085714bf9f177cb871d59
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-04-01 14:46:59 +0900

    chore(_tools): add npm build script

---
+++ _tools/meta.ts
@@ -0,0 +1,48 @@
+import { BuildOptions } from "https://deno.land/x/dnt@0.33.1/mod.ts";
+
+export const makeOptions = (version: string): BuildOptions => ({
+  test: false,
+  shims: {},
+  compilerOptions: {
+    lib: ["dom", "esnext"],
+  },
+  typeCheck: true,
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  package: {
+    name: "@httpland/xcto-middleware",
+    version,
+    description: "HTTP XCTO(X-Content-Type-Options) middleware",
+    keywords: [
+      "http",
+      "middleware",
+      "header",
+      "xcto",
+      "x-content-type-options",
+      "nosniiff",
+      "security",
+      "fetch-api",
+    ],
+    license: "MIT",
+    homepage: "https://github.com/httpland/xcto-middleware",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/httpland/xcto-middleware.git",
+    },
+    bugs: {
+      url: "https://github.com/httpland/xcto-middleware/issues",
+    },
+    sideEffects: false,
+    type: "module",
+    publishConfig: {
+      access: "public",
+    },
+  },
+  packageManager: "pnpm",
+  mappings: {
+    "https://deno.land/x/http_middleware@1.0.0/mod.ts": {
+      name: "@httpland/http-middleware",
+      version: "1.0.0",
+    },
+  },
+});
